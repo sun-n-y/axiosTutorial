7d47b0deb47c4437a0d3b3d7e506bcabe5c3e4b8
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-16 14:23:18 -0500

    add axios custom instance

--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,21 @@
 import { useEffect } from 'react';
+import axios from 'axios';
+import authFetch from '../axios/custom';
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const resp1 = await authFetch('/react-store-products');
+      const resp2 = await axios(randomUserUrl);
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>custom instance</h2>;
+  return <h2 className="text-center">custom instance</h2>;
 };
 export default CustomInstance;
