3126f2d2a9c93982f8267e42ed7089ee4ad1da89
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-16 14:41:15 -0500

    add axios inteceptors

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,16 +1,19 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/interceptors';
 
 const url = 'https://course-api.com/react-store-products';
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const resp = await authFetch('/react-store-productsd');
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+  return <h2 className="text-center">interceptors</h2>;
 };
 export default Interceptors;
